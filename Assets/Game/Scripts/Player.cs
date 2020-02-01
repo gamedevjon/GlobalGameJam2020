ae1bae4a8e3275a7a51e3d67982bd26de3fa81c1
GameDevHQ <47222323+gamedevjon@users.noreply.github.com>
2020-01-31 22:18:21 -1000

    Player Controller Implimented

---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _jumpHeight = 10.0f;
+    private float _gravity = 1.0f;
+    private float _yVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        Vector3 direction = new Vector3(h, 0, 0);
+        Vector3 velocity = direction * _speed;
+
+        if (Input.GetKeyDown(KeyCode.Space) && _controller.isGrounded == true)
+        {
+            _yVelocity = _jumpHeight;
+        }
+
+        _yVelocity -= _gravity;
+
+        velocity.y = _yVelocity;
+
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
